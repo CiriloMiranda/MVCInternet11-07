ef56b248f8be8cfabe47d61f0dc0b2db2e53d5bd
cirom <miranda.cirilo.6558@gmail.com>
2022-07-11 15:11:04 -0400

    Cambios de cuerdo al video3 controladores

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
 	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
+
+	public function test($id, $hola = "no se"){
+		//NOTA
+		//http://localhost/proyectonuevo/index.php/Welcome/test/1/hola
+		//http://localhost/proyectonuevo/index.php/CONTROLADOR/METODO/--> de aqui para adelante parametros para enviar
+		echo $id;
+		echo $hola;
+	}
+
 }
